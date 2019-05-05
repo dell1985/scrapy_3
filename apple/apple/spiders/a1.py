9cfaeccf2af143cfb5134df0e8f2551a1d7c4fe7
dell1985 <cn20060101@outlook.com>
2019-05-05 23:59:14 +0800

    h2.py 是根据交易日历文件，下载全部信息3

--- apple/apple/spiders/a1.py
+++ apple/apple/spiders/a1.py
@@ -12,24 +12,25 @@ class A1Spider(scrapy.Spider):
     start_urls = [url]
 
     def parse(self, response):
-        #这里取回期货品种的数据
-        #/html/body/div/table/tbody/tr/td[1]/b 这是第一个的期货品种名称
-        #所有期货名称的节点列表1/html/body/div/table/tbody/tr/td/
+        #这里取回期货[品种]和[合约]名称的数据
+        #品种是合约的组合
+        #期货品种（合约）
         name_form_heml = response.xpath('/html/body/div/table/tbody/tr/td/b/text()')
-        # 所有期货交易信息
-        jy_list=response.xpath('/html/body/div/table/tbody/tr/td/text()')
+        #期货交易信息
+        jydate_form_html=response.xpath('/html/body/div/table/tbody/tr/td/text()')
         # 用来存放所有的item字段
         i=1
 
         o=0
         name_list=[]
         for pz in name_form_heml:
-            
+            print(pz)
             #数据整理
             a=pz.extract()
-            a = (" ".join(a))
+            a = ("".join(a))
+            print(a)
             a = a.replace("\n", '')
-            a=a.replace(" ","")
+            #a=a.replace(" ","")
             a=a.replace("\xa0\xa0\xa0\xa0日期：","")
             a=a.replace("合约：", "")
             a=a.replace("品种：", "")
@@ -40,11 +41,20 @@ class A1Spider(scrapy.Spider):
 
             #单个期货名称数据添加到列表name_list中
             name_list.append(name)
+            print(name_list)
             i=i+1
             name_save=(name_list[o:o+1])
             print(name_save)
+            # print(jydate_list)
+            o = o + 1
+            # l=0
+            # for jydate in jydate_list:
+            #     l=l+1
+            #     print(jydate)
+
             yield {
                 '日期':b,
                 '品种': name_save
             }
-            o = o + 1
+        print(name_save)
+        print(name_list)
